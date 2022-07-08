6cb0b8b418dab4d9e0488b487b5c74ec219a61cf
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-07-08 13:02:56 -0500

    Update console.py

--- console.py
+++ console.py
@@ -24,12 +24,14 @@ class HBNBCommand(cmd.Cmd):
                        'Amenity', 'Place', 'Review']
 
     def do_quit(self, line):
-        """Quit command to exit the program.
+        """
+        Quit command to exit the program.
         """
         return True
 
     def do_EOF(self, line):
-        """Quit command to exit the program.
+        """
+        Quit command to exit the program.
         """
         return True
 
