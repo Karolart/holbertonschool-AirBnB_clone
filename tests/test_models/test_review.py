595adb75b6cdfe8389dc1f967215ed330a637c6f
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-07-07 19:06:57 -0500

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
-"""
-Test suits for amenities
-"""
-import os
-import models
-import unittest
-from datetime import datetime
+'''Unittest for State class'''
+from models.state import State
 from models.base_model import BaseModel
+from datetime import datetime
+import unittest
 
 
-class TestReview(unittest.TestCase):
-    """
-    Tests for amenities
-    """
+class TestUserClass(unittest.TestCase):
 
-    def test_name(self):
-        """
-        Tests for name inputs
-        """
-        pass
+	def setUp(self):
+		"""Set up for the doc tests"""
+		pass
+	def test_type(self):
+		"""test to check type of instance"""
+		pass
+
+	def test_user_id(self):
+		"""test to check user id"""
+		pass
+
+	def test_user_created(self):
+		"""test to check user created_at"""
+		pass
+
+	def test_user_updated(self):
+		"""test to check user updated_at"""
+		pass
+
+	def test_user_name(self):
+		"""test to check user name"""
+		pass
+
+	def test_str_output_user(self):
+		'''Tests for expected output'''
+		pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
