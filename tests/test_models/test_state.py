66f083c8a2d532543f134edcf45a70f80de7a963
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-07-09 23:08:18 -0500

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
-"""Test State"""
+"""Test suit for State"""
 from models.amenity import Amenity
-#!/usr/bin/python3
-"""
-Test suits for State
-"""
-import os
-import models
-import unittest
-from datetime import datetime
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+import pep8
+import unittest
 
 
-class TestState(unittest.TestCase):
+class Teststate(unittest.TestCase):
     """
-    Tests for State
+    Unittests for the State class.
     """
 
-    def test_name(self):
-        """
-        Tests for name inputs
-        """
-        pass
+    def test_pep8_conformance_state(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/state.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors")
 
+    def test_class(self):
+        """
+        Tests if input name is right.
+        """
+        state_test = State()
+        self.assertEqual(state_test.__class__.__name__, "State")
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_super_class(self):
+        """
+        Tests if Class inherits from BaseModel.
+        """
+        state_test = State()
+        self.assertEqual(state1.__class__.__name__, "State")
