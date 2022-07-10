e284d1c6a18e5a5b843950de422a441bc1b9af68
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-07-09 23:00:49 -0500

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Test suits for State
-"""
+"""Test State"""
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -10,28 +8,30 @@ from models.review import Review
 from models.state import State
 import pep8
 import unittest
-import os
 
 
-class TestState(unittest.TestCase):
+class Teststate(unittest.TestCase):
     """
-    Tests for state
+    Unittests for the State class.
     """
 
-   def test_pep8_conformance_state(self):
+    def test_pep8_conformance_state(self):
         """Test that we conform to PEP8."""
-        pass
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/state.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_name(self):
+    def test_class(self):
         """
-        Test if the name atribute is right.
+        Tests if class is named correctly.
         """
-        state_test = State()
-        self.assertEqual(state_test.__class__.__name__, "State")
+        state1 = State()
+        self.assertEqual(state1.__class__.__name__, "State")
 
     def test_father(self):
         """
         Tests if Class inherits from BaseModel.
         """
-        state_test = State()
+        state1 = State()
         self.assertEqual(state1.__class__.__name__, "State")
