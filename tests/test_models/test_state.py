cfa10d88b6e91985fe93e252d21c4a884c6d9b39
KarolDiaz <87344535+Karolart@users.noreply.github.com>
2022-07-09 22:57:44 -0500

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python3
 """
-Test suits for amenities
+Test suits for State
 """
-import os
-import models
-import unittest
-from datetime import datetime
+from models.amenity import Amenity
 from models.base_model import BaseModel
-
-
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+import pep8
+import unittest
+import os
 class TestState(unittest.TestCase):
     """
-    Tests for amenities
+    Tests for state
     """
 
+   def test_pep8_conformance_state(self):
+        """Test that we conform to PEP8."""
+        pass
+    
     def test_name(self):
         """
-        Tests for name inputs
+        Test if the name atribute is right.
         """
-        pass
+        state_test = State()
+        self.assertEqual(state_test.__class__.__name__, "State")
 
+    def test_father(self):
+        """
+        Tests if Class inherits from BaseModel.
+        """
+        state_test = State()
+        self.assertEqual(state1.__class__.__name__, "State")
 
 if __name__ == '__main__':
     unittest.main()
